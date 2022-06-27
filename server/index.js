6313a0c065c52e4e4874c6d4a6b52933dc24af18
Paul <thejunglife@gmail.com>
2022-06-27 16:46:52 -0700

    added mongoDB

--- server/index.js
+++ server/index.js
@@ -10,15 +10,27 @@ const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const PORT = process.env.PORT || 3000
-
 //middleware
 app.use(express.static(path.join(__dirname, 'client', 'build')))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+//Mongo DB credentials
+let user = process.env.USER
+let pass = process.env.PASS
+
+//Mongo DB
+const CONNECTION_URL = `mongodb+srv://${user}:${pass}@cluster0.dqfl1.mongodb.net/?retryWrites=true&w=majority`
+const port = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
-  res.send("Hello World")
+  res.send('Hello World')
 })
 
-app.listen(PORT, () => console.log('listening on port ' + PORT))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () => console.log(`Server running on port: ${port}`))
+  )
+  .catch((error) => console.log(error.message))
+
